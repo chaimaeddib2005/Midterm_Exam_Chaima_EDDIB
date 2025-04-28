af943f145f9d84df3ebbf0f9f836edf448103d07
Chaima EDDIB <145902851+chaimaeddib2005@users.noreply.github.com>
2025-04-28 13:54:55 +0000

    h

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
   devServer: {
-    webSocketServer: false
-  }
-})
+    client: {
+      webSocketURL: {
+        protocol: 'wss',
+        hostname: '10.0.10.103',
+        port: 8080,
+        pathname: '/ws',
+      }
+    }
+  }
+};
+
+
 module.exports = {
   publicPath: "/",
 };
